30949463aee53d278b0fa256a41feaebf345a2b6
Kratik <159747619+Kratik00@users.noreply.github.com>
2026-02-08 01:32:00 +0530

    Update configuration values in config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -2,11 +2,11 @@ from typing import List
 
 API_ID: int = 25318125
 API_HASH: str = "b29fb6a928e8b8a3308f8c2d3ba9cfb0""
-TOKEN: str = "8072650045:AAG9z8aR5GRdLaoGi0mOlvCza1YU3PVkluA"
+TOKEN: str = "8278824209:AAGJNf1sPQE8IMI0Q9pFejpNkkxFoO28jh8"
 
-log_chat: int = -1002230633058
-sub_chat: str = "LP_YAGAMI"
-sudoers: List[int] = [7376514183]
+log_chat: int = -1003738730540
+sub_chat: str = "normallydhundho"
+sudoers: List[int] = [7764674199]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
 # notes
@@ -15,3 +15,4 @@ prefixs: List[str] = ["/", "!", ".", "$", "-"]
 # 2. token fill with your bot_token get from @BotFather
 # 3. log_chat fill with the chat id of a group that you should create for the bot logger
 # 4. sub_chat fill with the channel username but don't include the '@' symbol
+
